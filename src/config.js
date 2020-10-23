46a15a0b8e0cefdcd85f4023b3e27bc72e57d489
mdmoin7 <45753713+mdmoin7@users.noreply.github.com>
2020-10-23 09:27:27 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const ADAL = {
+  auth: {
+    clientId: "CLIENT_ID",
+    authority: "https://login.microsoftonline.com/common",
+    postLogoutRedirectUri: window.location.origin,
+  },
+  storage: "sessionStorage",
+};
